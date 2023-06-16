Adding high and low res emulator extensions as well as changes to GPy emulator for variance fixing

data_loader.py
@@ -3,24 +3,20 @@ import matplotlib.pyplot as plt
 from scipy import interpolate
 import random
 import pandas as pd
-
-bins = 100
-#cutoff = (np.pi*1260)/1400
-
-#test_model = [3, 12, 18, 90, 138]
-#test_models = random.randint(0, 149)
-test_models = 50
-#test_model = 11
+from pade import pade
 
 
 class BXL_DMO_Pk:
 
-    def __init__(self, test_models, bins, cutoff = (0.025, 2.45), cosmo = np.arange(9), pk = 'swift', lin = 'class', boost = True):
+    def __init__(self, test_models, bins, cutoff = (0.025, 2.45), cosmo = np.arange(9), pk = 'swift', lin = 'class', boost = True, extrap = False, add_pade = False):
         self.test_models = test_models
+        self.bins = bins
         self.pk = pk
+        self.extrap = extrap
         
         #The parameters file
-        self.parameters = np.loadtxt('/storage/users/arijsalc/BAHAMAS_XL/DMO/slhs_nested_3x50_w0_m0p6_m1p2_wa_m1p6_p0p5_with_running_and_fgas_and_As.txt', skiprows=1, max_rows=150, usecols=cosmo)
+        #self.parameters = np.loadtxt('/storage/users/arijsalc/BAHAMAS_XL/DMO/slhs_nested_3x50_w0_m0p6_m1p2_wa_m1p6_p0p5_with_running_and_fgas_and_As.txt', skiprows=1, max_rows=150, usecols=cosmo)
+        self.parameters = np.loadtxt('./BXL_data/slhs_nested_3x50_w0_m0p6_m1p2_wa_m1p6_p0p5_with_running_and_fgas_and_As.txt', skiprows=1, max_rows=150, usecols=cosmo)
 
         #Normalize the design to be between 0,1
         self.design_max = np.max(self.parameters, 0)
@@ -36,23 +32,25 @@ class BXL_DMO_Pk:
         elif pk == 'nbk':
             array_size = 1023
         elif pk == 'nbk-rebin':
-            df = pd.read_csv("/storage/users/arijsalc/BAHAMAS_XL/DMO/boost_rebinned_21.csv")
+            df = pd.read_csv("./BXL_data/boost_rebinned_21.csv")
             array_size = 19
             
         self.k = np.zeros([150, array_size])
         self.P_k = np.zeros([150, array_size])
-        self.P_k_interp = np.zeros([150, bins])
-        self.P_k_nonlinear = np.zeros([150, bins])
+        self.P_k_interp = np.zeros([150, self.bins])
+        self.P_k_nonlinear = np.zeros([150, self.bins])
 
         self.low_k_cut = cutoff[0]
         self.high_k_cut = cutoff[1]
-        self.k_test = np.logspace(np.log10(self.low_k_cut), np.log10(self.high_k_cut), bins)
+        self.k_test = np.logspace(np.log10(self.low_k_cut), np.log10(self.high_k_cut), self.bins)
 
         if lin == 'camb':
             self.k_camb_linear = np.logspace(-3, np.log10(50), 300)
             self.P_k_camb_linear = np.loadtxt('/home/arijconl/BAHAMAS_XL/pk_lin_camb2022_slhs_nested_3x50_kmax_50_running_w0_m1p2_m0p6_wa_m1p6_p0p5.txt', skiprows=index, max_rows=1)
         elif lin == 'class':
             self.P_k_class_linear = np.zeros((150, 400, 2))
+        elif lin == 'rebin':
+            pass
     
         #This is used to build up k, the full P(k), linear P(k) and non-linear P(k)
         for i in range(150):
@@ -78,8 +76,19 @@ class BXL_DMO_Pk:
             elif pk == 'nbk-rebin':
                 self.k[i, :] = df['k'][19*i:19*(i+1)] 
                 self.P_k[i, :] = df['boost'][19*i:19*(i+1)] 
+
+            if extrap == False:
+                pass
+            else:
+                self.k_test = np.logspace(np.log10(self.low_k_cut), np.log10(10), self.bins)
+
+            if add_pade == False:
+                h = interpolate.interp1d(self.k[i, :], self.P_k[i, :], kind='cubic', fill_value="extrapolate" if extrap == True else 'nan')
+            elif add_pade == True:
+                print(self.k[i, -4:-1], self.P_k[i, -4:-1])
+                h = pade.fit(self.k[i, -4:-1], self.P_k[i, -4:-1])
+                print(self.k_test[-4:-1], h(self.k_test))
                 
-            h = interpolate.interp1d(self.k[i, :], self.P_k[i, :], kind='cubic')#, fill_value="extrapolate") #if pk == 'nbk' else 'nan')
             self.P_k_interp[i, :] = h(self.k_test)
         
             if lin == 'camb':
@@ -108,48 +117,42 @@ class BXL_DMO_Pk:
         
         #Log of non-linear boost
         self.Y_train = np.log10(self.P_k_nonlinear_train)
-        self.k_ind = np.linspace(0, 1, bins)
+        self.k_ind = np.linspace(0, 1, self.bins)
         #k_z0_test_array = np.repeat(k_z0_test, np.size(P_k_z0_nonlinear_train_log, 0), axis=0).reshape(-1, 100)
         #print(k_z0_test_array)
             
 
     def plot_k(self):
 
-        for i in range(150):
+        for i in range(50):
             plt.figure(3)
-            if i in range(50):
-                plt.plot(self.k_test, self.P_k_interp[i, :], color = 'tab:blue', label=('Models 000-049' if i==0 else None))
-            elif i in range(50, 100):
-                plt.plot(self.k_test, self.P_k_interp[i, :], color = 'tab:orange', label=('Models 050-099' if i==50 else None))
-            elif i in range(100, 150):
-                plt.plot(self.k_test, self.P_k_interp[i, :], color = 'tab:green', label=('Models 100-149' if i==100 else None))
+            plt.plot(self.k_test, self.P_k_interp[i, :], color = 'tab:blue', label=('Models 000-049' if i==0 else None))
+            plt.plot(self.k_test, self.P_k_interp[i+50, :], color = 'tab:orange', label=('Models 050-099' if i==0 else None))
+            plt.plot(self.k_test, self.P_k_interp[i+100, :], color = 'tab:green', label=('Models 100-149' if i==0 else None))
 
-            u=0
-            for t in self.k[i, :]:
-                if t <= max(self.P_k_class_linear[i, :, 0]):
-                    u+=1
-                    k_plot = self.k[i, :u]
-            e = interpolate.interp1d(self.P_k_class_linear[i, :, 0], self.P_k_class_linear[i, :, 1], kind='cubic')
-            p = e(k_plot)
+            ###Plotting function broken for now###
+            if self.extrap == False:
+                u=0
+                for t in self.k[i+(q*50), :]:
+                    if t <= max(self.P_k_class_linear[i+(q*50), :, 0]):
+                        u+=1
+                        k_plot = self.k[i, :u]
+                e = interpolate.interp1d(self.P_k_class_linear[i, :, 0], self.P_k_class_linear[i, :, 1], kind='cubic')
+                p = e(k_plot)
+                ######################################
+            elif self.extrap == True:
+                u=self.bins
 
             plt.figure(1)
-            if i in range(50):
-                plt.plot(k_plot, (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:blue', label=('Models 000-049' if i==0 else None))
-            elif i in range(50, 100):
-                plt.plot(k_plot, (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:orange', label=('Models 050-099' if i==50 else None))
-            elif i in range(100, 150):
-                plt.plot(k_plot, (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:green', label=('Models 100-149' if i==100 else None))
+            plt.plot((k_plot if self.pk != 'nbk-rebin' else self.k[i, :]), (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:blue', label=('Models 000-049' if i==0 else None))
+            plt.plot((k_plot if self.pk != 'nbk-rebin' else self.k[i+50, :]), (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i+50, :u]), color='tab:orange', label=('Models 050-099' if i==0 else None))
+            plt.plot((k_plot if self.pk != 'nbk-rebin' else self.k[i+100, :]), (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i+100, :u]), color='tab:green', label=('Models 100-149' if i==0 else None))
 
             plt.figure(2)
-            if i in range(50):
-                k_fund = (2*np.pi)/700
-                plt.plot(k_plot/k_fund, (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:blue', label=('Models 000-049' if i==0 else None))
-            elif i in range(50, 100):
-                k_fund = (2*np.pi)/1400
-                plt.plot(k_plot/k_fund, (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:orange', label=('Models 050-099' if i==50 else None))
-            elif i in range(100, 150):
-                k_fund = (2*np.pi)/350
-                plt.plot(k_plot/k_fund, (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:green', label=('Models 100-149' if i==100 else None))
+            k_fund_low = (2*np.pi)/1400
+            plt.plot((k_plot if self.pk != 'nbk-rebin' else self.k[i, :])/(k_fund_low*2), (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i, :u]), color='tab:blue', label=('Models 000-049' if i==0 else None))
+            plt.plot((k_plot if self.pk != 'nbk-rebin' else self.k[i+50, :])/k_fund_low, (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i+50, :u]), color='tab:orange', label=('Models 050-099' if i==0 else None))
+            plt.plot((k_plot if self.pk != 'nbk-rebin' else self.k[i+100, :])/(k_fund_low*4), (self.P_k[i, :u]/p if self.pk != 'nbk-rebin' else self.P_k[i+100, :u]), color='tab:green', label=('Models 100-149' if i==0 else None))
 
         plt.figure(1)
         plt.title('Boost function vs k for all models', fontsize=10, wrap=True)
@@ -199,7 +202,15 @@ class BXL_DMO_Pk:
             
         
 if __name__ == "__main__":
-    nbk_boost = BXL_DMO_Pk(test_models, bins, pk = 'nbk-rebin')
+    bins = 100
+    #cutoff = (np.pi*1260)/1400
+
+    #test_model = [3, 12, 18, 90, 138]
+    test_models = random.randint(0, 149)
+    #test_models = 50
+    #test_model = 11
+    
+    nbk_boost = BXL_DMO_Pk(test_models, bins, pk = 'nbk-rebin', lin = 'rebin', extrap = True, add_pade = True)
     print(nbk_boost.k)
     print(nbk_boost.P_k)
     print(nbk_boost.k_test)
@@ -208,4 +219,5 @@ if __name__ == "__main__":
     print(nbk_boost.Y_train)
     print(nbk_boost.X_test)
     print(nbk_boost.Y_test)
+    nbk_boost.plot_k()
 

gpy_emulator.py
@@ -9,33 +9,149 @@ linear = GPy.kern.Linear(9)
 polynomial = GPy.kern.Poly(9)
 RBF = GPy.kern.RBF(9)
 Bias = GPy.kern.Bias(9)
+ARD = GPy.kern.RBF(9, ARD=True)
 
 class gpy_emulator:
 
-    def __init__(self, P_k_data, kern):
+    def __init__(self, P_k_data, kern, var_fix = False):
         self.test_models = P_k_data.test_models
+        self.kern = kern
 
-        self.model = GPy.models.GPHeteroscedasticRegression(P_k_data.X_train, P_k_data.Y_train, kern)
+        self.model = GPy.models.GPHeteroscedasticRegression(P_k_data.X_train[:99, :], P_k_data.Y_train[:99, :], self.kern)
+        if var_fix == True:
+            self.model.het_Gauss.variance[:49] = 10
+            self.model.het_Gauss.variance[49:] = 0.01
+            self.model.het_Gauss.variance[49:].fix()
+        elif var_fix == False:
+            pass
         self.model.optimize()
-        self.y = self.model.predict(P_k_data.X_test.reshape(-1, 1))
+        self.y = 10**(self.model._raw_predict(P_k_data.X_test.reshape(1, -1))[0])
+        self.error = self.y/P_k_data.Y_test
 
-    def plot(self):
-        self.model.plot()
-        pb.errorbar(P_k_data.X_train, P_k_data.Y_train, yerr=np.array(self.model.likelihood.flattened_parameters).flatten(), fmt=None, ecolor='r', zorder=1)
+        return    
 
-        pb.title('GPy test 1 (kernel = ' + f"{kern}" + ')')
+    def plot(self, P_k_data, name):
+        pb.plot(P_k_data.k_test, P_k_data.Y_test, color='b', label=('True Non-linear P(k) for model_' + f"{self.test_models:03d}"))
+        pb.plot(P_k_data.k_test, self.y.reshape(-1,1), color='r', label=('Predicted Non-linear P(k) for model_' + f"{self.test_models:03d}"))
+
+        pb.title('GPy test (kernel = ' + f"{name}" + ')')
         pb.xlabel('k (1/Mpc)')
         pb.ylabel('P(k) (Mpc^3)')
         pb.xscale('log')
         pb.yscale('log')
+        pb.legend()
+        pb.savefig(f'./Plots/gpy_test_{name}.pdf', dpi=800)
+        pb.clf()
 
-        pb.savefig('./Plots/gpy_test_1.pdf', dpi=800)
+        
+        pb.hlines(y=1.000, xmin=-1, xmax=max(P_k_data.k_test)+1, color='k', linestyles='solid', alpha=0.5, label=None)
+        pb.hlines(y=0.990, xmin=-1, xmax=max(P_k_data.k_test)+1, color='k', linestyles='dashed', alpha=0.5, label='1% error')
+        pb.hlines(y=1.010, xmin=-1, xmax=max(P_k_data.k_test)+1, color='k', linestyles='dashed', alpha=0.5, label=None)
+        pb.hlines(y=0.950, xmin=-1, xmax=max(P_k_data.k_test)+1, color='k', linestyles='dotted', alpha=0.5, label='5% error')
+        pb.hlines(y=1.050, xmin=-1, xmax=max(P_k_data.k_test)+1, color='k', linestyles='dotted', alpha=0.5, label=None)
+        pb.plot(P_k_data.k_test, self.error.reshape(-1,1), label=('Residual error for model_' + f"{self.test_models:03d}"))
+        pb.title('GPy error test (kernel = ' + f"{name}" + ')')
+        pb.xlabel('k (1/Mpc)')
+        pb.ylabel('Residual error')
+        pb.xscale('log')
+        pb.legend()
+        pb.savefig(f'./Plots/gpy_test_error_{name}.pdf', dpi=800)
+        pb.clf()
+
+        return
 
 if __name__ == "__main__":
-    test_model = random.randint(0, 149)
-    nbk_boost = BXL_DMO_Pk(test_model, 100, pk = 'nbk-rebin')
-    gpy = gpy_emulator(nbk_boost, linear)
+    #test_model = random.randint(0, 149)
+    test_model = random.randint(0, 49)
+    nbk_boost = BXL_DMO_Pk(test_model, 100, pk='nbk-rebin', lin='rebin', extrap=True)
+    nbk_boost.plot_k()
+    gpy_rbf = gpy_emulator(nbk_boost, RBF)
+    gpy_ard = gpy_emulator(nbk_boost, ARD)
+    gpy_rbf_fix = gpy_emulator(nbk_boost, RBF, var_fix = True)
+    gpy_ard_fix = gpy_emulator(nbk_boost, ARD, var_fix = True)
+    #gpy_bias = gpy_emulator(nbk_boost, Bias)
+    #gpy_poly = gpy_emulator(nbk_boost, polynomial)
+    #gpy_lin = gpy_emulator(nbk_boost, linear)
+    #gpy_mlp = gpy_emulator(nbk_boost, mlp)
 
-    print(gpy.model.kern)
-    print(gpy.y)
+    #gpy_rbf_bias_a = gpy_emulator(nbk_boost, RBF+Bias)
+    #gpy_rbf_mlp_a = gpy_emulator(nbk_boost, RBF+mlp)
+    #gpy_rbf_poly_a = gpy_emulator(nbk_boost, RBF+polynomial)
+    #gpy_rbf_lin_a = gpy_emulator(nbk_boost, RBF+linear)
+
+    #gpy_rbf_bias_m = gpy_emulator(nbk_boost, RBF*Bias)
+    #gpy_rbf_mlp_m = gpy_emulator(nbk_boost, RBF*mlp)
+    #gpy_rbf_poly_m = gpy_emulator(nbk_boost, RBF*polynomial)
+    #gpy_rbf_lin_m = gpy_emulator(nbk_boost, RBF*linear)
     
+    print(gpy_rbf.model.kern)
+    print(gpy_rbf.error)
+    gpy_rbf.plot(nbk_boost, 'RBF')
+    print(gpy_ard.model.kern)
+    print(gpy_ard.error)
+    gpy_ard.plot(nbk_boost, 'RBF(ARD)')
+    gpy_rbf_fix.plot(nbk_boost, 'RBF fixed var')
+    gpy_ard_fix.plot(nbk_boost, 'RBF(ARD) fixed var')
+    #gpy_bias.plot(nbk_boost, 'Bias')
+    #gpy_poly.plot(nbk_boost, 'Polynomial')
+    #gpy_lin.plot(nbk_boost, 'Linear')
+    #gpy_mlp.plot(nbk_boost, 'MLP')
+    
+    #gpy_rbf_bias_a.plot(nbk_boost, 'RBF+Bias')
+    #gpy_rbf_mlp_a.plot(nbk_boost, 'RBF+MLP')
+    #gpy_rbf_poly_a.plot(nbk_boost, 'RBF+Polynomial')
+    #gpy_rbf_lin_a.plot(nbk_boost, 'RBF+Linear')
+
+    #gpy_rbf_bias_m.plot(nbk_boost, 'RBFxBias')
+    #gpy_rbf_mlp_m.plot(nbk_boost, 'RBFxMLP')
+    #gpy_rbf_poly_m.plot(nbk_boost, 'RBFxPolynomial')
+    #gpy_rbf_lin_m.plot(nbk_boost, 'RBFxLinear')
+
+    #mean_error0 = []
+    #for i in range(5):
+        #test_model = random.randint(0, 149)
+        #boost = BXL_DMO_Pk(test_model, 100, pk='nbk-rebin', lin='rebin')
+        #grbf = gpy_emulator(boost, RBF)
+        #gbias = gpy_emulator(nbk_boost, Bias)
+        #gpoly = gpy_emulator(nbk_boost, polynomial)
+        #glin = gpy_emulator(nbk_boost, linear)
+        #gmlp = gpy_emulator(nbk_boost, mlp)
+        
+        #grbf_bias_a = gpy_emulator(nbk_boost, RBF+Bias)
+        #grbf_mlp_a = gpy_emulator(nbk_boost, RBF+mlp)
+        #grbf_poly_a = gpy_emulator(nbk_boost, RBF+polynomial)
+        #grbf_lin_a = gpy_emulator(nbk_boost, RBF+linear)
+        
+        #grbf_bias_m = gpy_emulator(nbk_boost, RBF*Bias)
+        #grbf_mlp_m = gpy_emulator(nbk_boost, RBF*mlp)
+        #grbf_poly_m = gpy_emulator(nbk_boost, RBF*polynomial)
+        #grbf_lin_m = gpy_emulator(nbk_boost, RBF*linear)
+        
+        #mean_error0.append(grbf.error.reshape(-1,1))
+        
+        
+    #pb.hlines(y=1.000, xmin=-1, xmax=nbk_boost.high_k_cut+1, color='k', linestyles='solid', alpha=0.5, label=None)
+    #pb.hlines(y=0.990, xmin=-1, xmax=nbk_boost.high_k_cut+1, color='k', linestyles='dashed', alpha=0.5, label='1% error')
+    #pb.hlines(y=1.010, xmin=-1, xmax=nbk_boost.high_k_cut+1, color='k', linestyles='dashed', alpha=0.5, label=None)
+    #pb.hlines(y=0.950, xmin=-1, xmax=nbk_boost.high_k_cut+1, color='k', linestyles='dotted', alpha=0.5, label='5% error')
+    #pb.hlines(y=1.050, xmin=-1, xmax=nbk_boost.high_k_cut+1, color='k', linestyles='dotted', alpha=0.5, label=None)
+    #pb.plot(nbk_boost.k_test, gpy_rbf.error.reshape(-1,1), label=('kernel = RBF'), linestyle='solid', color='b')
+    #pb.plot(nbk_boost.k_test, gpy_lin.error.reshape(-1,1), label=('kernel = Linear'), linestyle='solid', color='g')
+    #pb.plot(nbk_boost.k_test, gpy_poly.error.reshape(-1,1), label=('kernel = Polynomial'), linestyle='solid', color='r')
+    #pb.plot(nbk_boost.k_test, gpy_mlp.error.reshape(-1,1), label=('kernel = MLP'), linestyle='solid', color='m')
+    #pb.plot(nbk_boost.k_test, gpy_bias.error.reshape(-1,1), label=('kernel = Bias'), linestyle='solid', color='c')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_bias_a.error.reshape(-1,1), label=('kernel = RBF+Bias'), linestyle='dashed', color='c')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_lin_a.error.reshape(-1,1), label=('kernel = RBF+Linear'), linestyle='dashed', color='g')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_poly_a.error.reshape(-1,1), label=('kernel = RBF+Polynomial'), linestyle='dashed', color='r')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_mlp_a.error.reshape(-1,1), label=('kernel = RBF+MLP'), linestyle='dashed', color='m')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_bias_m.error.reshape(-1,1), label=('kernel = RBF*Bias'), linestyle='dotted', color='c')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_lin_m.error.reshape(-1,1), label=('kernel = RBF*Linear'), linestyle='dotted', color='g')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_poly_m.error.reshape(-1,1), label=('kernel = RBF*Polynomial'), linestyle='dotted', color='r')
+    #pb.plot(nbk_boost.k_test, gpy_rbf_mlp_m.error.reshape(-1,1), label=('kernel = RBF*MLP'), linestyle='dotted', color='m')
+    #pb.title('GPy error test (model_' + f"{test_model}" + ')' )
+    #pb.xlabel('k (1/Mpc)')
+    #pb.ylabel('Residual error')
+    #pb.xscale('log')
+    #pb.legend(fontsize=5, ncols=4)
+    #pb.savefig(f'./Plots/gpy_test_error_comparison.pdf', dpi=800)
+    #pb.clf()