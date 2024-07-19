f24ff283f2dc21a504425b683e3d11b2db04bead
Jonah Conley <ARIJCONL@ljmu.ac.uk>
2024-07-19 11:50:24 +0100

    Added test_cosmology condition and used .copy() to protect variables

--- data_loader.py
+++ data_loader.py
@@ -11,7 +11,8 @@ import glob
 
 class bahamasXLDMOData:
 
-    def __init__(self, test_cosmology=False, bins=100, cutoff=(-1, 10), cosmo_params=np.arange(9), pk='nbk-rebin-std', lin='class', boost=True, holdout=False, sigma8=False, plot_hypercube=False, weight_k=True):
+    def __init__(self, test_cosmology=False, bins=100, cutoff=(-1, 10), cosmo_params=np.arange(9), pk='nbk-rebin-std', lin='class', boost=True, log=True, holdout=False, sigma8=False, plot_hypercube=False, weight_k=True):
+
         self.test_cosmology = test_cosmology
         self.bins = bins
         self.pk = pk
@@ -20,38 +21,45 @@ class bahamasXLDMOData:
         self.low_k_cut = cutoff[0]
         self.high_k_cut = cutoff[1]
         self.holdout = holdout
+        self.log = log
+
         
         #The parameters file
         self.parameters = np.loadtxt('./BXL_data/slhs_nested_3x50_w0_m0p6_m1p2_wa_m1p6_p0p5_with_running_and_fgas_and_As.txt', skiprows=1, max_rows=150, usecols=cosmo_params)
         if sigma8 == True:
             sig8 = np.loadtxt('./BXL_data/slhs_nested_3x_Om_fb_h_ns_sigma8_w0_wa_Omnuh2_alphas_fgasfb.txt', max_rows=150, usecols=4)
-            self.parameters[:, 4] = sig8
+            self.parameters_sig8 = np.hstack((self.parameters.copy(), sig8.reshape(-1,1)))
+
 
         if plot_hypercube == True:
             labels = {r'$\Omega_m$':[.20,.25,.30,.35], r'$f_b$':[.14,.15,.16,.17], r'$h_0$':[.64,.7,.76], r'$n_s$':[.95,.97,.99], r'$\sigma_8$':[0.72,0.80,0.88], r'$w_0$':[-.7,-.9,-1.1], r'$w_a$':[.2,-.5,-1.2], r'$\Omega_{\nu}h^2$':[.005,.003,.001], r'$\alpha_s$':[.025,0,-.025]}
             
-            hypercube = hypercube_plot(data=[self.parameters[:50, :], self.parameters[50:100, :], self.parameters[100:, :]], parameter_labels=labels, save_to='./Plots/BXL_hypercube.png', title='BAHAMAS XL Latin hypercube design')
+            hypercube = hypercube_plot(data=[self.parameters_sig8[:50, :], self.parameters_sig8[50:100, :], self.parameters_sig8[100:, :]], parameter_labels=labels, save_to='./Plots/BXL_hypercube.png', title='BAHAMAS XL Latin hypercube design')
 
-        if pk == 'powmes' and weight_k == True:
-            correction_factor = [1.2761424, 1.1154015, 1.0447197, 1.0151449, 1.0195166, 1.0112120]
-                
+
+        self.correction_factor = [1.2761424, 1.1154015, 1.0447197, 1.0151449, 1.0195166, 1.0112120]
+
+        
         #Normalize the design to be between 0,1
         self.design_max = np.max(self.parameters, 0)
         self.design_min = np.min(self.parameters, 0)
         self.parameters_norm = (self.parameters-self.design_min)/(self.design_max-self.design_min)
-    
+
+        
         #Split into test and train data
         if isinstance(holdout, bool):
-            self.X_train = self.parameters_norm
-            if test_cosmology != False:
+            self.X_train = self.parameters_norm.copy()
+            if not isinstance(test_cosmology, bool):
                 self.X_test = (self.test_cosmology-self.design_min)/(self.design_max-self.design_min)
         else:
-            self.X_test = self.parameters_norm[self.holdout, :]
-            self.X_train = np.delete(self.parameters_norm, self.holdout, axis=0)
+            self.X_test = self.parameters_norm[self.holdout, :].copy()
+            self.X_train = np.delete(self.parameters_norm.copy(), self.holdout, axis=0)
 
+            
         def extract_series(group):
             return {'k': group['k'].values, 'Pk': group['boost'].values, 'model': group['model'].iloc[0], 'modes': group['N_modes'].values if pk == 'nbk-rebin-std' else None}
 
+        
         if pk == 'nbk-rebin' or pk == 'nbk-rebin-std':
             if pk == 'nbk-rebin':
                 df = pd.read_csv("./BXL_data/boost_rebinned_21.csv")
@@ -80,6 +88,7 @@ class bahamasXLDMOData:
                         self.array_size = x-1
                         break
 
+                    
         if pk == 'nbk-rebin-std':
             self.k_test = arrays_per_model[0]['k']
         else:
@@ -93,8 +102,8 @@ class bahamasXLDMOData:
                 powmes_low_res_model = np.loadtxt(glob.glob('/mnt/aridata1/users/arijsalc/BAHAMAS_XL/DMO/model_' + f"{50:03d}" + '_N1260_L*_DMO/power_spectra/power_matter_0122.txt')[0], skiprows = 20, usecols = 1)
                 powmes_high_res_model = np.loadtxt(glob.glob('/mnt/aridata1/users/arijsalc/BAHAMAS_XL/DMO/model_' + f"{100:03d}" + '_N1260_L*_DMO/power_spectra/power_matter_0122.txt')[0], skiprows = 20, usecols = 1)
                 if weight_k == True:
-                    random_model[:6, 0] = random_model[:6, 0]*correction_factor
-                    powmes_low_res_model[:6] = powmes_low_res_model[:6]*correction_factor
+                    random_model[:6, 0] = random_model[:6, 0]*self.correction_factor
+                    powmes_low_res_model[:6] = powmes_low_res_model[:6]*self.correction_factor
                 
                 minimum_k = min(random_model[:, 0] if random_int in range(100, 150) else powmes_high_res_model)
                 maximum_k = max(random_model[:, 0] if random_int in range(50, 100) else powmes_low_res_model)
@@ -118,10 +127,12 @@ class bahamasXLDMOData:
 
             print(self.low_k_cut, self.high_k_cut)
             self.k_test = np.logspace(np.log10(self.low_k_cut), np.log10(self.high_k_cut), self.bins)
+
             
         if self.lin == 'camb':
             self.k_camb_linear = np.tile(np.logspace(-3, np.log10(50), 300), (150,1))
             self.P_k_camb_linear = np.loadtxt('./BXL_data/pk_lin_camb2022_slhs_nested_3x50_kmax_50_running_w0_m1p2_m0p6_wa_m1p6_p0p5.txt')
+
         elif self.lin == 'class':
             class_directory = glob.glob('/mnt/aridata1/users/arijsalc/BAHAMAS_XL/DMO/model_*_N1260_L*_DMO/class_linear_spectra_z_0.txt')
             for index, directory in enumerate(class_directory):
@@ -132,11 +143,12 @@ class bahamasXLDMOData:
                     self.P_k_class_linear = np.zeros([150, P_k_lin_size])
                 self.k_class_linear[index, :] = np.loadtxt(directory, skiprows=2, usecols=0)
                 self.P_k_class_linear[index, :] = np.loadtxt(directory, skiprows=2, usecols=2)
+
         elif self.lin == 'rebin':
             pass
 
+        
         #This is used to build up k, the full P(k), linear P(k) and non-linear P(k)
-
         self.k = np.zeros([150, self.array_size])
         self.P_k = np.zeros([150, self.array_size])
         self.P_k_interp = np.zeros([150, len(self.k_test)])
@@ -149,7 +161,7 @@ class bahamasXLDMOData:
                 self.k[index, :] = np.loadtxt(directory, skiprows = 20, usecols = 1)[:self.array_size]
                 self.P_k[index, :] = np.loadtxt(directory, skiprows = 20, usecols = 2)[:self.array_size]
                 if weight_k == True:
-                    self.k[index, :6] = self.k[index, :6]*correction_factor
+                    self.k[index, :6] = self.k[index, :6]*self.correction_factor
 
         elif pk == 'nbk':
             training_directory = glob.glob('/mnt/aridata1/users/arijsalc/BAHAMAS_XL/DMO/model_*_N1260_L*_DMO/PS/PS_k_078.csv')
@@ -166,13 +178,14 @@ class bahamasXLDMOData:
                 
 
         if pk == 'nbk-rebin-std':
-            self.P_k_interp = self.P_k
+            self.P_k_interp = self.P_k.copy()
         else:
             interpolation_function = {}
             for i in range(len(self.k)):
                 interpolation_function[i] = interpolate.interp1d(self.k[i, :], self.P_k[i, :], kind='cubic')
                 
                 self.P_k_interp[i, :] = interpolation_function[i](self.k_test)
+
 
         self.linear_interpolation_function = {}
         if self.lin == 'camb':
@@ -191,42 +204,48 @@ class bahamasXLDMOData:
                 self.P_k_linear_interp[l, :] = self.linear_interpolation_function[l](self.k_test)
 
         if pk == 'nbk-rebin' or pk == 'nbk-rebin-std':
-            self.P_k_nonlinear = self.P_k_interp
+            self.P_k_nonlinear = self.P_k_interp.copy()
         else:
             if boost == True:
-                self.P_k_nonlinear = self.P_k_interp / self.P_k_linear_interp
+                self.P_k_nonlinear = self.P_k_interp.copy() / self.P_k_linear_interp.copy()
             elif boost == False:
-                self.P_k_nonlinear = self.P_k_interp
-        
+                self.P_k_nonlinear = self.P_k_interp.copy()
+
+        self.nan_mask = np.isnan(self.P_k_nonlinear)
         try:
             logged_array = np.log10(self.P_k_nonlinear)
         except ValueError:
-            nan_mask = np.isnan(self.P_k_nonlinear)
-            self.P_k_nonlinear[nan_mask] = 0
+            self.P_k_nonlinear[self.nan_mask] = 0
 
         if isinstance(holdout, bool):
-            self.Y_train = np.log10(self.P_k_nonlinear)
+            self.Y_test = np.ones((len(self.k_test)))
+            if log == True:
+                self.Y_train = np.log10(self.P_k_nonlinear)
+            else:
+                self.Y_train = self.P_k_nonlinear.copy()
         else:
-            self.Y_test = self.P_k_nonlinear[self.holdout, :]
-            self.Y_train = np.log10(np.delete(self.P_k_nonlinear, self.holdout, axis=0))
+            self.Y_test = self.P_k_nonlinear[self.holdout, :].copy()
+            if log == True:
+                self.Y_train = np.log10(np.delete(self.P_k_nonlinear.copy(), self.holdout, axis=0))
+            else:
+                self.Y_train = np.delete(self.P_k_nonlinear.copy(), self.holdout, axis=0)
 
         return
             
 
     def plot_k(self, file_type='png'):
 
-        #linear_med_limit=linear_low_limit=linear_high_limit=self.array_size+1
-        linear_limit = []
+        
         if self.lin == 'camb' or self.lin == 'class':
-            k_limit = 30
+            linear_limit = []
+            k_limit = 30            
+            for i in range(150):
+                for index, k in enumerate(self.k[i, :]):
+                    if k >= k_limit:
+                        linear_limit.append(index)
+                        break
         else:
-            k_limit = 1000
-            
-        for i in range(150):
-            for index, k in enumerate(self.k[i, :]):
-                if k >= k_limit:
-                    linear_limit.append(index)
-                    break
+            linear_limit = [self.array_size+1 for _ in range(150)]
         
         for i in range(50):
             plt.figure(3)
@@ -283,17 +302,19 @@ class bahamasXLDMOData:
     
     def extend_data(self, pad=False):
 
-        self.nan_mask = np.isnan(self.P_k_nonlinear)
         print(self.nan_mask)
         print(self.nan_mask[0,:],self.nan_mask[50,:],self.nan_mask[100,:])
+
         if pad == 'emu':
             self = gpyLowResEmulator(self).data
             self = gpyHighResEmulator(self).data
+
         elif pad == 'pade':
             for f in range(50):
                 pade_func((3, 8, 13), self.k_test, self.P_k_nonlinear[f, :], (2, 15, 15), self.k_test, self.P_k_nonlinear[f, :])
                 pade_func((2, 7, 12), self.k_test, self.P_k_nonlinear[f+50, :], (1, 14, 15), self.k_test, self.P_k_nonlinear[f+50, :])
                 pade_func((4, 8, 13), self.k_test, self.P_k_nonlinear[f+100, :], (3, 15, 15), self.k_test, self.P_k_nonlinear[f+100, :])
+
         elif pad == False:
             pass
         
@@ -306,7 +327,7 @@ if __name__ == "__main__":
 
     test_models = random.randint(0, 149)
     
-    nbk_boost = bahamasXLDMOData(pk='powmes', lin='class', holdout=test_models)
+    nbk_boost = bahamasXLDMOData(pk='nbk-rebin-std', lin='rebin', holdout=test_models, log=False)
     print(nbk_boost.P_k_nonlinear)
     nbk_boost.extend_data('emu')
     print(nbk_boost.P_k_nonlinear)
@@ -321,12 +342,3 @@ if __name__ == "__main__":
     nbk_boost.plot_k()
     quit()
         
-    plt.plot(nbk_boost.k_test, nbk_boost.Y_test)
-    plt.xscale('log')
-    #plt.yscale('log')
-    plt.title('FLAMINGO models relative to the fiducial run (1Gpc med res)', fontsize=10, wrap=True)
-    plt.xlabel(r'$k \: [1/Mpc]$')
-    plt.ylabel(r'$P_{fiducial}(k)/P_{i}(k)$')
-    plt.legend()
-    plt.savefig('./Plots/test.pdf', dpi=800)
-    plt.clf()
